453f7635248449befd2aba1a596494f4e9cceead
Renzo Chávez <rlchavezc@outlook.com>
2020-03-07 16:35:12 -0500

    - Minor update changes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
+
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
+
 import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
+import 'firebase/firestore';
+
+import Swal from 'sweetalert2';
 import { User } from './user.model';
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -19,19 +24,18 @@ export class AuthService {
   initAuthListener() {
     this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
       console.log( fbUser );
-      console.log( fbUser.email );
     });
   }
 
-  crearUsuario(nombre: string, email: string, password: string) {
+  crearUsuario( nombre: string, email: string, password: string ) {
     this.afAuth
       .createUserWithEmailAndPassword(email, password)
       .then( resp => {
-        // console.log( resp );
-        const user:User = {
-          nombre: nombre,
-          email: resp.user.email,
+        // console.log(resp);
+        const user: User = {
           uid: resp.user.uid,
+          nombre: nombre,
+          email: resp.user.email
         };
         this.afDB.doc(`${ user.uid }/usuario`)
             .set( user )
@@ -41,7 +45,7 @@ export class AuthService {
       })
       .catch( error => {
         console.log( error );
-        Swal.fire('Error registro', error.message, 'error');
+        if (error.code !== 'auth/email-already-in-use') Swal.fire('Error registro', error.message, 'error');
       });
   }
 
@@ -54,7 +58,7 @@ export class AuthService {
       })
       .catch( error => {
         console.log(error);
-        Swal.fire('Error login', error.message, 'error');
+        Swal.fire('Error en el login', error.message, 'error');
       })
   }
 
